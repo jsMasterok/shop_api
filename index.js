b17a289c7880692eee94e4d73ce7e8f86871b300
ArtemDEV <artem@mobius.team>
2024-09-04 19:43:16 +0300

    delete origin

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import cors from "cors";
 
-const app = express()
-  .use(express.json())
-  .use(
-    cors({
-      origin: "https://crm.sitniks.com/open-api",
-    })
-  );
+const app = express().use(express.json()).use(cors());
 
 app.use(
   "/",
