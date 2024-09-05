b1749c33cb6a1efa750179fec401568ac395c687
ArtemDEV <artem@mobius.team>
2024-09-05 17:49:19 +0300

    delete proxy

--- index.js
+++ index.js
@@ -8,14 +8,71 @@ app.get("/", (req, res) => {
   res.send("BestWishesAPI");
 });
 
+app.get("/sales-channels", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "/https://crm.sitniks.com/open-api/sales-channels",
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data from API:", error);
+    res.status(500).send("Error fetching data");
+  }
+});
+
 app.get("/products", async (req, res) => {
   try {
     const response = await axios.get(
       "https://crm.sitniks.com/open-api/products",
       {
         headers: {
-          Authorization: `Bearer ${process.env.BEARER_TOKEN}`, // Токен передается здесь
-          "Content-Type": "application/json", //
+          Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data from API:", error);
+    res.status(500).send("Error fetching data");
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await axios.get(
+      `https://crm.sitniks.com/open-api/products/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data from API:", error);
+    res.status(500).send("Error fetching data");
+  }
+});
+
+app.post("/orders", async (req, res) => {
+  try {
+    const requestData = req.body;
+    const response = await axios.get(
+      "/https://crm.sitniks.com/open-api/orders",
+      requestData,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+          "Content-Type": "application/json",
         },
       }
     );
